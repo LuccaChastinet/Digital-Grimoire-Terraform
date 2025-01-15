af116c588de02ea09174b2e8cc0e45c2129911b4
Lucca Maia Chastinet <luccachastinet20@gmail.com>
2025-01-14 22:06:20 -0300

    Creating Artifact Registry Resource

--- artifactregistry.tf
+++ artifactregistry.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "artifact_registry" {
-  location      = "us-central1"
+  location      = var.region
   repository_id = "digital-grimoire-ar"
   description   = "Artifact Registry to store containers for the digital grimoire Project"
   format        = "DOCKER"
